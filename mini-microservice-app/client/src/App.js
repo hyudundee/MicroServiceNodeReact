a98ce0573cdfb055cf62bd7f1c5c9e0b42bb9471
hyudundee <hyudundee@gmail.com>
2021-03-09 20:09:58 -0800

    client updated

---
+++ mini-microservice-app/client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PostCreate from './PostCreate'
+const func = () => {
+  return <div className="container">
+    <h1>Create Post</h1>
+    <PostCreate />
+  </div>
+}
+
+export default func;
